fef82bcb779bbd40a0fa335b8542009a092eccb7
dimadsky@gmail.com <dimadsky@gmail.com>
2017-03-28 22:49:58 +0300

    fixed remarks

--- 01_ES6/js/main.js
+++ 01_ES6/js/main.js
@@ -6,7 +6,7 @@ class Pokemon{
         this.level = level;
     }
     show () {
-        console.log(this.name, this.level);
+        console.log(`Pokemon: ${this.name} with level: ${this.level}`);
     }
     valueOf(){
         return this.level;
@@ -15,26 +15,37 @@ class Pokemon{
 
 class PokemonList extends Array{
     constructor (name, ...pokemons){
-        super();
+        super(...pokemons);
         this.name = name;
-        for (let p of pokemons) {
-            this.push(p);
-        }
     }
     add(name, level){
         this.push(new Pokemon (name, level));
     }
     show(){
-        let show = `In list ${this.name} - ${this.length} pokemons: `;
+        console.log('Pokemon list:');
         for (let pokemon of this) {
-            show += pokemon.name + `(${pokemon.level}) `;
+            pokemon.show();
         }
-        console.log(show);
     }
     max(){
-        return Math.max(...this);
+        let max = -1;
+        this.forEach(p=>{
+            if(max < p.valueOf()){
+                max = p;
+            }
+        });
+        return max
+        //Math.max(...this);
     }
-    static changeList(indexFrom, from, to){
+    static changeList(nameFrom, from, to){
+        let indexFrom = from.findIndex((el, i)=>{
+            if(el.name == nameFrom){
+                return true;
+            }
+        });
+        if(indexFrom == -1){
+            console.log('No pokemon with such name')
+        }
         to.push(from[indexFrom]);
         from.splice(indexFrom, 1);
     }
@@ -45,14 +56,12 @@ let p1 = new Pokemon('Tor', 31),
     p3 = new Pokemon('Kiri', 19),
     p5 = new Pokemon();
 
-p5.show();
-
 let found  = new PokemonList ('found', p1, p2),
     lost  = new PokemonList ('lost', p3, p5);
 
 lost.add('Newbie');
-console.log('Max level', found.max());
-PokemonList.changeList(0, lost, found);
+found.max().show();
+PokemonList.changeList('Kiri', lost, found);
 found.show();
 lost.show();
 
