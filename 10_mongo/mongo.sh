b2b40fc217cb30c8b4bc66314a9efa77a619f4f6
dimadsky@gmail.com <dimadsky@gmail.com>
2017-04-27 19:00:44 +0300

    done

---
+++ 10_mongo/mongo.sh
@@ -0,0 +1,21 @@
+#Добавлять список имён в коллекцию;
+#Выводить этот список;
+#Изменять несколько имён на другие;
+#Отображать изменённый список;
+#Удалять новые имена из п.3.
+
+use testdb
+show collections
+db.users.insert({name:'Anya', gender:'f'});
+db.users.insert({name:'Vanya', gender:'f'});
+db.users.insert({name:'Tanya', gender:'f'});
+db.users.insert({name:'Vova'});
+db.users.insert({name:'Vova', gender:'f'});
+db.users.count();
+
+db.users.update({name:"Vanya"},{$set:{gender:'m'}})
+db.users.update({name:"Vanya"},{$set:{gender:'m'}}, {multi:true})
+db.users.find()
+
+db.users.remove({name:"Vanya"});
+db.users.find()
